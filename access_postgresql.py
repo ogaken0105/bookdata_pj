9807f7ab47c4f7cc10e1b3376ed3baab8b3b8513
ogaken0105 <k.ogawa9315@gmail.com>
2021-06-29 15:50:26 +0000

    modify sql sentence

--- access_postgresql.py
+++ access_postgresql.py
@@ -1,6 +1,5 @@
-#insert_dataは共通化できるところがありそう。
-
 import psycopg2
+import pandas as pd
 from pprint import pprint
 
 
@@ -25,116 +24,77 @@ con = postgres_connection(hostname='books_database',\
 
 def read_sql(con, select_sql):
     with con.cursor() as cur:
-        df = pd.read_sql(sql=select_sql, con=self.con)
-        pprint(df)
+        df = pd.read_sql(select_sql, con)
+        return df
 
-def create_table(con, create_table_sql):
+def operate_database(con, sql_sentence, sql_value=None):
     with con:
         con.set_client_encoding('utf-8') 
-        with con.cursor() as cur:
+        try:
+            cur = con.cursor()
         # テーブルを作成する SQL を準備
-            sql = create_table_sql
-            # SQL を実行し、テーブル作成
-            cur.execute(sql)
+            try:
+                cur.execute(sql_sentence,sql_value)
+            except psycopg2.IntegrityError:
+                    con.rollback()
+            else:
+                con.commit()
+        except Exception as e:
+            print(f'Eroor{e}')
         # コミットし、変更を確定する
         con.commit()
     # 接続を閉じる
     con.close()
 
-def insert_data(con,df):
-    with con:
-        con.set_client_encoding('utf-8') 
+def make_insert_books_sql_sentence(df):
+    bookdata_list = df.values.tolist()
+    for book_data in bookdata_list:
 
-        try:
-            cur = con.cursor()
+        isbn = book_data[0]
+        series_num = book_data[1]
+        genre_code = book_data[2]
+        genre_taget = book_data[3]
+        genre_category = book_data[4]
+        genre_content_code = book_data[5]
+        title = book_data[6]
+        author = book_data[7]
+        description = book_data[8]
+        release_date = book_data[9]
+        coverpicture = book_data[10]
 
-            bookdata_list = df.values.tolist()
-            for book_data in bookdata_list:
+        sql_sentence = 'INSERT INTO books VALUES (%s,%s,%s,\
+                                                %s,%s,%s,\
+                                                %s,%s,%s,\
+                                                %s,%s)'
+        sql_value = (isbn,\
+                    series_num,\
+                    genre_code,\
+                    genre_taget,\
+                    genre_category,\
+                    genre_content_code,\
+                    title,\
+                    author,\
+                    description,\
+                    release_date,\
+                    coverpicture\
+                    )
+        yield sql_sentence, sql_value
 
-                isbn = book_data[0]
-                series_num = book_data[1]
-                genre_code = book_data[2]
-                genre_taget = book_data[3]
-                genre_category = book_data[4]
-                genre_content_code = book_data[5]
-                title = book_data[6]
-                author = book_data[7]
-                description = book_data[8]
-                release_date = book_data[9]
-                coverpicture = book_data[10]
+def make_insert_author_sql_sentence(df):
+    bookdata_list = df.values.tolist()
 
-                #Noneが文字列で入ってしまっている
-                '''
-                sql_sentence = f'INSERT INTO books VALUES ({isbn},\
-                                                            {series_num},\
-                                                            {genre_code},\
-                                                            {genre_taget},\
-                                                            {genre_category},\
-                                                            {genre_content_code},\
-                                                            '{title}',\
-                                                            '{author}',\
-                                                            '{description}',\
-                                                            {release_date},\
-                                                            '{coverpicture}'\
-                                                            );'
-                '''
-                
-                try:
-                    sql_sentence = 'INSERT INTO books VALUES (%s,%s,%s,\
-                                                            %s,%s,%s,\
-                                                            %s,%s,%s,\
-                                                            %s,%s)'
-                    cur.execute(sql_sentence,(isbn,\
-                                            series_num,\
-                                            genre_code,\
-                                            genre_taget,\
-                                            genre_category,\
-                                            genre_content_code,\
-                                            title,\
-                                            author,\
-                                            description,\
-                                            release_date,\
-                                            coverpicture\
-                                            ))
+    
+    for book_data in bookdata_list:
 
-                except psycopg2.IntegrityError:
-                    con.rollback()
-                else:
-                    con.commit()
-        except Exception as e:
-            print(f'Eroor{e}')
+        #ここは関数を入れる
+        #authorsデータベースの取得
+        #取得したデータベースの中から、
+        author = book_data[7]
+        author_num = numbering.author_numbering(author)
+        sql_sentence = 'INSERT INTO authors VALUES (%s,%s)'
+        sql_value = (author_num,author)
+        yield sql_sentence, sql_value
 
-    con.close()
-
-
-def insert_author(con, df):
-    with con:
-        con.set_client_encoding('utf-8') 
-
-        try:
-            cur = con.cursor()
-
-            bookdata_list = df.values.tolist()
-            for book_data in bookdata_list:
-
-                #ここは関数を入れる
-                #authorsデータベースの取得
-                #取得したデータベースの中から、
-                author = book_data[7]
-                author_num = numbering.author_numbering(author)
-                
-                try:
-                    sql_sentence = 'INSERT INTO authors VALUES (%s,%s)'
-                    cur.execute(sql_sentence,(author_num,author,))
-
-                except psycopg2.IntegrityError:
-                    con.rollback()
-                else:
-                    con.commit()
-        except Exception as e:
-            print(f'Eroor{e}')
-
-    con.close()
 
 create_table_sql = '''
                 CREATE TABLE Books  (
@@ -152,6 +112,7 @@ create_table_sql = '''
                     PRIMARY KEY (ISBN)
                 );
                 '''
+
 create_author_table_sql =  '''
                 CREATE TABLE authors  (
                     Author_num CHAR(10),
